b8a4349d97b8497870c5a053a103e1b9073982ad
Emerson1911 <1284662@clases.edu.sv>
2023-09-30 22:21:26 +0200

    Login de seleccion listo

--- WindowsFormsApp1/Formadministrador.cs
+++ WindowsFormsApp1/Formadministrador.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace WindowsFormsApp1
 {
@@ -15,6 +16,42 @@ namespace WindowsFormsApp1
         public Formadministrador()
         {
             InitializeComponent();
+        }
+
+        SqlConnection Conexion;
+        SqlDataReader Leer;
+
+        public void leer()
+        {
+            string CadenadeConexion = "Data Source=DESKTOP-FUUIGHB\\SQLEXPRESS;Initial Catalog=usuarios;integrated security=true";
+            Conexion = new SqlConnection(CadenadeConexion);
+            Conexion.Open();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            leer();
+            string Insertar = "INSERT INTO probedores VALUES(@Nombre,@Producto,@Telefono,@Dirección,@Correo)";
+            SqlCommand Comando1 = new SqlCommand(Insertar, Conexion);
+            Comando1.Parameters.AddWithValue("@Nombre", textBox1.Text);
+            Comando1.Parameters.AddWithValue("@Producto", textBox2.Text);
+            Comando1.Parameters.AddWithValue("Telefono", textBox3.Text);
+            Comando1.Parameters.AddWithValue("@Dirección", textBox4.Text);
+            Comando1.Parameters.AddWithValue("@Correo", textBox5.Text);
+            Comando1.ExecuteNonQuery();
+            Conexion.Close();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
